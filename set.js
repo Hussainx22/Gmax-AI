bd59c90cbf9c8565581fd34c0cada2ca17357203
CybertechTz <gmaxtechteam2024@gmail.com>
2025-01-05 21:46:55 +0300

    Update set.js

--- set.js
+++ set.js
@@ -7,27 +7,50 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Sir Gmax",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Sir Gmax",              
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'GMAX_MD',
-    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/o973mw.jpeg',
-    MODE: process.env.PUBLIC_MODE || "yes",
-    PM_PERMIT: process.env.PM_PERMIT || 'yes',
-    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+module.exports = { session: process.env.SESSION_ID || 'BELTAH-MD;;;',
+    PREFIXE: process.env.PREFIX || ",",
+    GITHUB : process.env.GITHUB|| 'https://github.com/CybertechTz/Gmax-AI',
+    OWNER_NAME : process.env.OWNER_NAME || "Gmax Tech",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255622053093",
+    ANTICALL: process.env.ANTICALL || "non",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTOREAD_MESSAGES: process.env.AUTOREAD_MESSAGES || "non",
+    AUTO_REACT: process.env.AUTO_REACTION || "non",
+    GURL: process.env.GURL  || "",
+    WEBSITE :process.env.GURL || "",
+    CAPTION : process.env.CAPTION || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ BELTAH-MD",
+    BOT : process.env.BOT_NAME || 'GMAX AI-MD',
+    URL : process.env.BOT_MENU_LINKS || '',
+    MODE: process.env.PUBLIC_MODE || "no",
+    TIMEZONE: process.env.TIMEZONE || "Africa/Tanzania", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY || null,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.PM_CHATBOT || 'no',
+    //GPT : process.env.OPENAI_API_KEY || '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',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
     ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    CHATBOT : process.env.PM_CHATBOT || 'no',  
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
-   
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
